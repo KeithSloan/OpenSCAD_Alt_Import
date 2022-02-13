0033cb6e71b17bde3a3d0221a446a5fd6b19c1a6
Keith Sloan <keith@sloan-home.co.uk>
2022-02-13 21:55:57 +0000

    Stack

--- OpenSCADMinkowski.py
+++ OpenSCADMinkowski.py
@@ -8,6 +8,23 @@ def minkowski(p):
        print(dir(obj2))
        return obj1
 
+    # - For minkowski Just indicate first shape needs editing
+
+    if len(p[6]) == 2 :
+       # return just first object
+       #print(dir(p[6][0]))
+       #print(dir((p[6][1]).ViewObject))
+       #print(p[6][0].TypeId)
+       p[6][0].ViewObject.ShapeColor = (1.,0.,0.)
+       setObjColor(p[6][0],(1.,0.,0.))
+       setOutListColor(p[6][0],(1.,0.,0.))
+       #print(p[6][1].TypeId)
+       p[6][1].ViewObject.ShapeColor = (0.,1.,0.)
+       setObjColor(p[6][1],(0.,1.,0.))
+       setOutListColor(p[6][1],(1.,0.,0.))
+       #p[6][1].ViewObject.hide()
+       p[0] = [p[6][0]]
+
     else:
        return CGALFeatureObject(p[1],p[6],p[3])
 
