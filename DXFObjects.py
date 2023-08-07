2d901ff80bf0e45129708f7f6feb05cc6895ce4d
mconsidine <matt@considine.net>
2023-08-07 08:38:48 -0400

    Update DXFObjects.py
    Attempt at fixing up DXF calls

--- DXFObjects.py
+++ DXFObjects.py
@@ -94,10 +94,10 @@ class DXFBase:
             #print(f"New Shape {newShp}")
             #print(f"Old Shape {shp}")
             #obj.Shape = newShp
-            if isinstance(shp, tuple):
-                obj.Shape = shp[0]
-            else:
-                obj.Shape = shp
+            #if isinstance(shp, tuple): #MattC commented out 20230807 0838
+            #    obj.Shape = shp[0]
+            #else:
+            #    obj.Shape = shp
         else:
             print(f"Shape is None")
             obj.Shape = Part.Shape()
