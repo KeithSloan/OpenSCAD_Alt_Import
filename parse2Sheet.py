c4ff38f8a7e80c8b7e54bd0db22c8ab19d0715d1
Keith Sloan <keithsloan52@icloud.com>
2025-03-23 18:14:58 +0000

    Stack

---
+++ parse2Sheet.py
@@ -0,0 +1,77 @@
+#***************************************************************************
+#*                                                                         *
+#*   Copyright (c) 2025 Keith Sloan <keith@sloan-home.co.uk>               *
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+#*   as published by the Free Software Foundation; either version 2 of     *
+#*   the License, or (at your option) any later version.                   *
+#*   for detail see the LICENCE text file.                                 *
+#*                                                                         *
+#*   This program is distributed in the hope that it will be useful,       *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#*   GNU Library General Public License for more details.                  *
+#*                                                                         *
+#*   You should have received a copy of the GNU Library General Public     *
+#*   License along with this program; if not, write to the Free Software   *
+#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+#*   USA                                                                   *
+#*                                                                         * 
+#*   Acknowledgements :                                                    *
+#*                                                                         *
+#***************************************************************************
+__title__="FreeCAD OpenSCAD Workbench - Parse SCAD to FreeCAD Sheet"
+__author__ = "Keith Sloan <keith@sloan-home.co.uk>"
+__url__ = ["http://www.sloan-home.co.uk/ImportSCAD"]
+
+#import FreeCAD, Part, Draft, io, os, sys, xml.sax
+import FreeCAD, FreeCADGui, os
+if FreeCAD.GuiUp:
+    import FreeCADGui
+    gui = True
+else:
+    print("FreeCAD Gui not present.")
+    gui = False
+
+# Save the native open function to avoid collisions
+if open.__module__ in ['__builtin__', 'io']:
+    pythonopen = open
+
+params = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/OpenSCAD")
+printverbose = params.GetBool('printverbose',False)
+print(f'Verbose = {printverbose}')
+
+def parseFunction(filename):
+    from scadParser.scad_parser import parseFileAndPrintGlobals
+    print(f"Parse File {filename}")
+    parseFileAndPrintGlobals(filename)
+
+def parse2SheetFunction(filename, doc):
+    from scadParser.scad_parser import parseFileAndPrintGlobals
+    print(f"Parse to Sheet {filename}")
+    parseFileAndPrintGlobals(filename)
+
+def open(filename, currentdoc=None):
+    "called when freecad opens a file."
+    FreeCAD.Console.PrintMessage('Creating Object from : '+filename+'\n')
+    pathText = os.path.splitext(os.path.basename(filename))
+    objectName  = pathText[0]
+    filePath = pathText[1]
+    print(f"Create Object {objectName} path {filename}")
+    #doc = FreeCAD.ActiveDocument
+    if currentdoc is None:
+        doc=FreeCAD.newDocument(objectName)    
+    else:
+        doc=FreeCAD.getDocument(currentdoc)       
+    if doc is None:
+        doc=FreeCAD.newDocument(filename)
+    
+    print(f"SCAD Parse to Spreadsheet")
+    parseFunction(filename)
+    #FreeCAD.ActiveDocument.recompute()
+    #FreeCADGui.SendMsgToActiveView("ViewFit")
+
+def insert(filename, currentdoc):
+    open(filename, currentdoc)
+
